d0662dfa3f29e8781030d6bc1a6e3a80398ce7df
Johnny Kaime <johnnyhou789@gmail.com>
2019-03-05 15:24:26 +0200

    oof'd
    not efficient but sort of working

--- 212-213/212.pl
+++ 212-213/212.pl
@@ -3,27 +3,60 @@
 use strict;
 use warnings;
 use CGI;
-undef $/;
 
 my %messageHash;
+my @nameArray;
+my $position;
+
 my $fileName = 'messages.txt';
 open(my $fh, '+<', $fileName) or die "Could not open $fileName";
 
 while(my $row = <$fh>){
-	print $row;
-	print "\nend of row\n";
 	my @row = split('>>',$row);
-	
 	$messageHash{$row[0]} = $row[1];
+	push(@nameArray, $row[0]);
 }
-close($fh);
+close($fh); 
+
+undef $/;
+my $webfile = '212.html';
+open( FILE, "< $webfile" ) or die "Could not open $webfile";
+my $template = <FILE>;
+close( FILE );
+
+my $script = new CGI;
+
+my $message = $script->param( 'message' );
+my $username = $script->param( 'username' );
+my $next = $script->param( 'Nex' );
+my $previous = $script->param( 'Pre' );
+
+
+if(defined $username){
+	$template =~ s/<dummy>(.*?)<\/dummy>/$message/s;
+	$template =~ s/{dumName}(.*?){\/dumName}/$username/s;
+	$messageHash{$username} = $message;
+	push(@nameArray,$username);
+	$position = scalar @nameArray;
+}elsif(defined $next){
+	my $name = $nameArray[$position];
+	my $mess = $messageHash{$nameArray[$position]};
+	$template =~ s/<dummy>(.*?)<\/dummy>/$mess/s;
+	$template =~ s/{dumName}(.*?){\/dumName}/$name/s;
+	$position++;
+}elsif(defined $previous){
+	my $name = $nameArray[$position];
+	my $mess = $messageHash{$nameArray[$position]};
+	$template =~ s/<dummy>(.*?)<\/dummy>/$mess/s;
+	$template =~ s/{dumName}(.*?){\/dumName}/$name/s;
+	$position--;
+}
+
+open(my $fh, '>', $fileName) or die "Could not open $fileName";
 foreach my $key (keys %messageHash){
-	print $key," ",$messageHash{$key};
+	print $fh $key,">>",$messageHash{$key},"\n";
 }
+close($fh); 
 
-
-#my $webfile = '212.html';
-#open( FILE, "< $webfile" ) or die "Could not open $webfile";
-#my $template = <FILE>;
-#close( FILE );
-
+print $script->header( -Cache_Control => 'private' );
+print $template;
